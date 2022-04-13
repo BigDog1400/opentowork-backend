5bbbdc7a99c2fc64adf2005ab524c32601e780a3
Isaac Peña <isaac.pr99@gmail.com>
2022-04-13 01:44:40 -0400

    feat: update AppError Class to receive data object with key value pairs of errors

--- src/common/app-error.ts
+++ src/common/app-error.ts
@@ -3,18 +3,14 @@ export default class AppError extends Error {
   status?: string;
   message: string;
   isOperational = true;
-  attributes: Record<string, any>;
+  data?: Record<string, any>;
 
-  constructor(
-    message: string,
-    statusCode: number,
-    attributes?: Record<string, any>
-  ) {
+  constructor(message: string, statusCode: number, data?: Record<string, any>) {
     super(message); // call the parent constructor
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true; // this is for the dev to know if the error is operational or not
-    this.attributes = attributes;
+    this.data = data;
     Error.captureStackTrace(this, this.constructor);
   }
 }

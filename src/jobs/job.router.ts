77e95be9ee37d2cb9b46b950c39a740198498533
Isaac Peña <isaac.pr99@gmail.com>
2022-04-15 19:44:43 -0400

    feat: add new route to job model

--- src/jobs/job.router.ts
+++ src/jobs/job.router.ts
@@ -1,9 +1,13 @@
 import express, { Router } from "express";
+import { auth } from "../middleware/auth";
 import * as JobService from "./job.service";
 
 const jobsRouter = express.Router();
 
-jobsRouter.route("/").get(JobService.getAll);
+jobsRouter
+  .route("/")
+  .get(JobService.getAll)
+  .post(auth.protect, auth.restrictTo("Company"), JobService.createJob);
 
 jobsRouter.route("/:id").get(JobService.getJob);
 

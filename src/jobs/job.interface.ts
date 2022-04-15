c6e7f23ca6e6cc8a7930f9aff458a412cc7e776d
Isaac Peña <isaac.pr99@gmail.com>
2022-04-15 19:43:42 -0400

    feat: update job interface

---
+++ src/jobs/job.interface.ts
@@ -0,0 +1,20 @@
+import { Types } from "mongoose";
+
+export interface IBaseJob {
+  _id: Types.ObjectId;
+  user_id: Types.ObjectId;
+  username: string;
+  title: string;
+  location: string[];
+  job_type: string;
+  minimum_experience: string;
+  required_skills: string[];
+  salary_range: {
+    min: number;
+    max: number;
+  };
+  description: string;
+  show_salary: boolean;
+  active: boolean;
+  instructions: Record<any, unknown>;
+}

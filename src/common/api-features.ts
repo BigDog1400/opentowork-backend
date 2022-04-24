8532b1a71ff2ada52dc028494b72bc6f021efab9
Isaac Peña <isaac.pr99@gmail.com>
2022-04-24 01:13:37 -0400

    feaT: update class to use aggregation pipeline instead of normal query

--- src/common/api-features.ts
+++ src/common/api-features.ts
@@ -1,6 +1,6 @@
-import { Query } from "mongoose";
+import { Aggregate } from "mongoose";
 class APIFeatures {
-  query: Query<any[], any, {}, any>;
+  query: Aggregate<any>;
   queryString: Record<string | number, string | number>;
 
   constructor(query, queryString) {
@@ -13,8 +13,20 @@ class APIFeatures {
     excludedFields.forEach((el) => delete queryObj[el]);
     // Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    this.query.find(JSON.parse(queryStr));
+    queryStr = queryStr.replace(
+      /\b(gte|gt|eq|lte|in|lt)\b/g,
+      (match) => `$${match}`
+    );
+    const queryObjParsed = JSON.parse(queryStr);
+    // we should check if any of the key in queryObjParsed has $in filter
+    // if so, we should convert the string value separated by comma to array
+    Object.keys(queryObjParsed).forEach((el) => {
+      if (queryObjParsed[el].hasOwnProperty("$in")) {
+        queryObjParsed[el]["$in"] = queryObjParsed[el]["$in"].split(",");
+      }
+    });
+
+    this.query.match(queryObjParsed);
     return this;
   }
 
@@ -30,11 +42,16 @@ class APIFeatures {
 
   limitFields() {
     if (this.queryString.fields) {
-      const fields = String(this.queryString.fields).split(",").join(" ");
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select("-__v");
+      const fields = {};
+      String(this.queryString.fields)
+        .split(",")
+        .forEach((el) => (fields[el] = 1));
+      this.query = this.query.project(fields);
     }
+    // TODO: create a slug field in the models to be able to use this
+    // else {
+    //   this.query = this.query.project({ _id: 0 });
+    // }
     return this;
   }
 
@@ -42,7 +59,11 @@ class APIFeatures {
     const page = Number(this.queryString.page) * 1 || 1;
     const limit = Number(this.queryString.limit) * 1 || 100;
     const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
+
+    this.query = this.query.facet({
+      total: [{ $count: "count" }],
+      docs: [{ $skip: skip }, { $limit: limit }],
+    });
     return this;
   }
 }

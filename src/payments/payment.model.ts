30374a9b348f65d62aa15b55b07b97e158903f11
Isaac Peña <isaac.pr99@gmail.com>
2022-05-07 00:53:00 -0400

    feat: add static method to Payment Schema to update job related

--- src/payments/payment.model.ts
+++ src/payments/payment.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { Job } from "../jobs/job.model";
 import { IPayment } from "./payment.interface";
 
 const paymentSchema = new Schema<IPayment>({
@@ -11,6 +12,23 @@ const paymentSchema = new Schema<IPayment>({
   status: { type: String, default: "PENDING_TO_PAY" },
   free_charge: { type: Boolean, default: false },
   order_id: Schema.Types.Mixed,
+});
+
+paymentSchema.statics.activeJobRelated = async function (jobId) {
+  await Job.findByIdAndUpdate(jobId, {
+    active: true,
+  });
+};
+
+paymentSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne().clone();
+  next();
+});
+
+paymentSchema.post(/^findOneAnd/, async function (result) {
+  if (result.status === "PAYED") {
+    await this.r.constructor.activeJobRelated(this.r.job);
+  }
 });
 
 export const Payment = model("Payment", paymentSchema);

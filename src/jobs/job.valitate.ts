1098dcace957ae3b9a4d1142c170cf6f73575a9a
Isaac Peña <isaac.pr99@gmail.com>
2022-05-07 00:52:35 -0400

    feat: add Job Schema

---
+++ src/jobs/job.valitate.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+import { IBaseJob } from "./job.interface";
+
+export const JobSchema = Joi.object<IBaseJob>({
+  active: Joi.boolean(),
+  description: Joi.string().required(),
+  instructions: Joi.string().required(),
+  job_type: Joi.string().required(),
+  location: Joi.array().items(Joi.string()).required(),
+  minimum_experience: Joi.string().required(),
+  required_skills: Joi.array().items(Joi.string()).required(),
+  salary_range: Joi.object({
+    max: Joi.number().required(),
+    min: Joi.number().required(),
+  }).required(),
+  show_salary: Joi.boolean().required(),
+  title: Joi.string().required(),
+});
